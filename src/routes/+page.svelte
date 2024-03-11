d064a127dfce7eee245f391a97b1b9a4100cfcf1
Henry Baldwin <henrymbaldwin@gmail.com>
2024-03-11 14:39:24 -0400

    Added header and graph to home page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Header from '$lib/components/Header.svelte';
+    import Graph from '$lib/components/Graph.svelte';
+</script>
+<Header />
+<Graph />
